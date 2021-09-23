08fe077fb80c9f873dadcc3abd7099aa10c2dfa1
Alex Casas <buddy123.at@gmail.com>
2021-09-23 11:25:37 -0500

    initial commit, unity HDRP created

---
+++ Assets/Input/InputActions.cs
@@ -0,0 +1,176 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Input/InputActions.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @InputActions : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @InputActions()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""InputActions"",
+    ""maps"": [
+        {
+            ""name"": ""Gameplay"",
+            ""id"": ""f933a333-3b40-40c2-9008-28d87be38e3a"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Value"",
+                    ""id"": ""1a01f7ea-6afb-4fc1-becb-6864dfc974a5"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""2D Vector"",
+                    ""id"": ""130874e7-1ed4-44eb-823f-3a2dbe74fafc"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""c3f26f9d-f540-4088-b82d-822a28cfc6fd"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""7bd0fc09-751a-4327-927b-c88ec1b79d1d"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""2d9d6000-e914-4325-a583-22c395f5860a"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""0ccd90cf-980d-4131-87fe-38fc670c37c0"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Gameplay
+        m_Gameplay = asset.FindActionMap("Gameplay", throwIfNotFound: true);
+        m_Gameplay_Move = m_Gameplay.FindAction("Move", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Gameplay
+    private readonly InputActionMap m_Gameplay;
+    private IGameplayActions m_GameplayActionsCallbackInterface;
+    private readonly InputAction m_Gameplay_Move;
+    public struct GameplayActions
+    {
+        private @InputActions m_Wrapper;
+        public GameplayActions(@InputActions wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_Gameplay_Move;
+        public InputActionMap Get() { return m_Wrapper.m_Gameplay; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(GameplayActions set) { return set.Get(); }
+        public void SetCallbacks(IGameplayActions instance)
+        {
+            if (m_Wrapper.m_GameplayActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+            }
+            m_Wrapper.m_GameplayActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+            }
+        }
+    }
+    public GameplayActions @Gameplay => new GameplayActions(this);
+    public interface IGameplayActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Prefabs/Player/PlayerControler.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControler : MonoBehaviour
+{
+    [SerializeField] Transform GroundCheck;
+    [SerializeField] float WalkingSpeed = 5f;
+    [SerializeField] float RotationSpeed = 24f;
+    [SerializeField] float GroundCheckRadius = 0.1f;
+    [SerializeField] LayerMask GroundCheckMask;
+    InputActions inputActions;
+    Vector2 MoveInput;
+    Vector3 Velocity;
+    float Gravity = -9.81f;
+    CharacterController characterController;
+
+    bool IsOnGround()
+    {
+        return Physics.CheckSphere(GroundCheck.position, GroundCheckRadius, GroundCheckMask);
+    }
+    private void Awake()
+    {
+        inputActions = new InputActions();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        inputActions.Gameplay.Move.performed += MoveInputUpdated;
+        inputActions.Gameplay.Move.canceled += MoveInputUpdated;
+    }
+
+    void MoveInputUpdated(InputAction.CallbackContext ctx)
+    {
+        MoveInput = ctx.ReadValue<Vector2>();
+    }
+
+
+    void Update()
+    {
+        if(IsOnGround())
+        {
+            Velocity.y = -0.2f;
+        }
+
+        Velocity.x = GetPlayerDesiredMoveDir().x * WalkingSpeed;
+        Velocity.z = GetPlayerDesiredMoveDir().z * WalkingSpeed;
+        Velocity.y += Gravity * Time.deltaTime;
+        characterController.Move(Velocity * Time.deltaTime);
+        UpdateRotation();
+    }
+
+    Vector3 GetPlayerDesiredMoveDir()
+    {
+        return new Vector3(-MoveInput.y, 0f, MoveInput.x).normalized;
+    }
+
+    void UpdateRotation()
+    {
+        Vector3 PlayerDesiredDir = GetPlayerDesiredMoveDir();
+        if(PlayerDesiredDir.magnitude == 0)
+        {
+            PlayerDesiredDir = transform.forward;
+        }
+        Quaternion DesiredRotation = Quaternion.LookRotation(PlayerDesiredDir, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, DesiredRotation, Time.deltaTime * RotationSpeed);
+    }
+}
